f37772ce9fdd6714573b5c69997ab1c5a52583d1
Nunes <diego7n@gmail.com>
2022-05-04 21:31:22 -0300

    prototype login screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import "./App.css";
+import Logo from "./logo.png";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Hello World!</p>
+        <p>Bem-vindo à Taqtile</p>
+        <img src={Logo} width="100px" height="100px" />
+        <input placeholder="E-mail" />
+        <input placeholder="Senha" />
+        <button>Entrar</button>
       </header>
     </div>
   );
